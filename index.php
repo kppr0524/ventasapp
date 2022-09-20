bf8506c7e34eb7cd910de8523162cc9ce264f078
Katherine Puello <kppr.0524@gmail.com>
2022-09-20 15:15:32 -0500

    Primeros ajustes a una tabla, adicion de archivos accion_page y conexion

--- index.php
+++ index.php
@@ -1,14 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Ventas App</title>
+    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="stylesheet" href="styles/css/bootstrap.min.css">
+    <link rel="stylesheet" href="">
 </head>
+
 <body>
-    <form action="POST">
-        
-    </form>
+    <script href="styles/js/bootstrap.bundle.min.js" rossorigin="anonymous"></script>
+    <h2>Inventario de Vehículos</h2>
+    <table>
+         <tr>
+            <th>Modelo</th>
+            <th>Marca</th>
+            <th>Fecha Venta</th>
+            <th>Cantidad Stock</th>
+            <th>Valor unidad</th>
+         </tr>
+         <tr>
+            <td>CX-3</td>
+            <td>Mazda</td>
+            <td>15-09-2022</td>
+            <td>2</td>
+            <td>56.000.000</td>
+         </tr>
+         <tr>
+            <td>CX-3</td>
+            <td>Mazda</td>
+            <td>15-09-2022</td>
+            <td>2</td>
+            <td>56.000.000</td>
+         </tr>
+         <tr>
+            <td>CX-3</td>
+            <td>Mazda</td>
+            <td>15-09-2022</td>
+            <td>2</td>
+            <td>56.000.000</td>
+         </tr>
+    </table>
 </body>
+
 </html>
